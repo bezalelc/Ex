cd66c83445971593b260519821281db5cfdc16a3
bb <bezalelcohen1@gmail.com>
2021-12-19 21:50:09 +0200

    remove section of month with checkbox

--- HTML/Ex4/TaxCalculator.js
+++ HTML/Ex4/TaxCalculator.js
@@ -1,1 +1,13 @@
-
+$(document).ready(function () {
+    let p;
+    $('input[type=checkbox]').change(function () {
+        if ($(this).is(':checked')) {
+            p = $("#months").detach();
+        } else {
+            if (p) {
+                p.appendTo("main");
+                p = null;
+            }
+        }
+    });
+});
