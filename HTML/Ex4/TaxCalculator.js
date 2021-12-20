e29dacc7a794a48e30b65463238501f2fb332d74
bb <bezalelcohen1@gmail.com>
2021-12-20 20:24:19 +0200

    tax result style

--- HTML/Ex4/TaxCalculator.js
+++ HTML/Ex4/TaxCalculator.js
@@ -1,7 +1,6 @@
+let tax = [[74640, 10], [32388, 14], [64788, 20], [66948, 31], [21509, 35], [11922, 47]];
 
-let tax = [[74640,10],[32388,14],[64788,20],[66948,31],[21509,35],[11922,47]];
-
-function calculate(p){
+function calculate(p) {
 
     let BaseSalary = $("#BaseSalary");
     let BonusPoint = $("#BonusPoint");
@@ -9,45 +8,47 @@ function calculate(p){
     let sum = 0;
     let myTax = 0;
 
-    if(p){
-        sum = parseFloat(BaseSalary[0].value)*12;
+    if (p) {
+        sum = parseFloat(BaseSalary[0].value) * 12;
         alert(sum);
-    }else{
-        for(let i = 0; i < 12; i++){
-            if(salary[i].value !== "")
+    } else {
+        for (let i = 0; i < 12; i++) {
+            if (salary[i].value !== "")
                 sum += parseFloat(salary[i].value);
         }
     }
     let i = 0;
-    while (sum > 0){
-        if(i === 6){
-            myTax += (sum/50);
+    while (sum > 0) {
+        if (i === 6) {
+            myTax += (sum / 50);
             break;
         }
-        if(sum > tax[i][0])
-            myTax += (tax[i][0]*tax[i][1])/100;
+        if (sum > tax[i][0])
+            myTax += (tax[i][0] * tax[i][1]) / 100;
         else
-            myTax += (sum*tax[i][1])/100;
+            myTax += (sum * tax[i][1]) / 100;
 
         sum -= tax[i][0];
         i++;
     }
-    if(BonusPoint !== null) {
+    if (BonusPoint !== null) {
         myTax -= BonusPoint[0].value * 2580;
-        if(myTax < 0)
+        if (myTax < 0)
             myTax = 0;
     }
     return myTax;
 }
 
-function add(myTax){
+function add(myTax) {
     let txt = "Total Tax is: " + myTax.toString();
     let a = $(".Tax");
-    if(a[0]){
+    if (a[0]) {
         a[0].innerHTML = txt;
-    }else{
+    } else {
         var newDiv = $("<div><div/>").addClass("Tax");
         newDiv[0].innerHTML = txt;
+        newDiv[0].style.fontsize = "x-large";
+        newDiv[0].style.color = "red";
         $("main:last").append(newDiv);
     }
 }
@@ -65,7 +66,7 @@ $(document).ready(function () {
         }
     });
 
-    $("#Calculate").click(function() {
+    $("#Calculate").click(function () {
         let myTax = calculate(p);
         add(myTax);
     });
