9a2f455a181523ae7186a3ea6ae8017d67b2f77e
Barak <baraik9@gmail.com>
2021-12-20 19:40:17 +0200

    bu

--- HTML/Ex4/TaxCalculator.js
+++ HTML/Ex4/TaxCalculator.js
@@ -1,13 +1,72 @@
+
+let tax = [[74640,10],[32388,14],[64788,20],[66948,31],[21509,35],[11922,47]];
+
+function calculate(p){
+
+    let BaseSalary = $("#BaseSalary");
+    let BonusPoint = $("#BonusPoint");
+    let salary = $(".input-text");
+    let sum = 0;
+    let myTax = 0;
+
+    if(p){
+        sum = parseFloat(BaseSalary[0].value)*12;
+    }else{
+        for(let i = 0; i < 12; i++){
+            if(salary[i].value !== "")
+                sum += parseFloat(salary[i].value);
+        }
+    }
+    let i = 0;
+    while (sum > 0){
+        if(sum > tax[i][0])
+            myTax += (tax[i][0]*tax[i][1])/100;
+        else
+            myTax += (sum*tax[i][1])/100;
+
+        if(i === 6){
+            myTax += (sum/50);
+            break;
+        }
+        sum -= tax[i][0];
+        i++;
+    }
+    if(BonusPoint !== null) {
+        myTax -= BonusPoint[0].value * 2580;
+        if(myTax < 0)
+            myTax = 0;
+    }
+    return myTax;
+}
+
+function add(myTax){
+    let txt = "Total Tax is: " + myTax.toString();
+    let a = $(".Tax");
+    if(a[0]){
+        a[0].innerHTML = txt;
+    }else{
+        var newDiv = $("<div><div/>").addClass("Tax");
+        newDiv[0].innerHTML = txt;
+        $("main:last").append(newDiv);
+    }
+}
+
 $(document).ready(function () {
     let p;
-    $('input[type=checkbox]').change(function () {
+    $('[type=checkbox]').change(function () {
         if ($(this).is(':checked')) {
-            p = $("#months").detach();
+            p = $("#months").fadeOut("slow");
         } else {
             if (p) {
-                p.appendTo("main");
+                p.fadeIn("slow");
                 p = null;
             }
         }
     });
+
+    $("#Calculate").click(function() {
+        let myTax = calculate(p);
+        add(myTax);
+    });
+
 });
